5d7cae5a6406fccb28108d1596138f564cf277d1
Lê Nguyễn Anh Tuấn <leanhtuan2911206@gmail.com>
2025-11-17 18:27:58 +0700

    Lưu lại code trước khi pull

--- views/main.php
+++ views/main.php
@@ -6,9 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <title><?= $title ?? 'Home' ?></title>
-
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="/assets/css/admin.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 </head>
